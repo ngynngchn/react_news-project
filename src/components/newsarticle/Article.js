890afa758a56c11309a5d797046733d53d645450
Hanni <ngynngchn@gmail.com>
2023-02-27 16:03:26 +0100

    added noreferrer' as netlify suggested

--- src/components/newsarticle/Article.js
+++ src/components/newsarticle/Article.js
@@ -23,7 +23,7 @@ const Article = ({
 				<h3>{title}</h3>
 				<p>{description}</p>
 			</article>
-			<a href={url} target="_blank">
+			<a href={url} target="_blank" rel="noreferrer">
 				Link to article
 			</a>
 		</section>
